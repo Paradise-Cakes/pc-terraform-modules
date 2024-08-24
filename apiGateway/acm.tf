468865b508bfe774fdc97840ec83291ced4475fa
Anthony Viera <anthonyviera4@gmail.com>
2024-08-23 20:59:08 -0500

    fix: add depends on for domain name

--- apiGateway/acm.tf
+++ apiGateway/acm.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate_validation" "paradise_cakes" {
+resource "aws_acm_certificate_validation" "validation" {
   certificate_arn         = var.api_acm_certificate_arn
   validation_record_fqdns = [for record in aws_route53_record.api_validation_record : record.fqdn]
 }
