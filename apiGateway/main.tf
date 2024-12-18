d91415eaacef8657127f194746b97a651c8678f7
Anthony Viera <anthonyviera4@gmail.com>
2024-12-17 20:43:04 -0600

    feat: authorizer stuff

--- apiGateway/main.tf
+++ apiGateway/main.tf
@@ -15,7 +15,8 @@ resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
 }
 
 resource "aws_api_gateway_stage" "stage" {
@@ -118,4 +119,12 @@ resource "aws_api_gateway_integration_response" "cors" {
     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,DELETE,PATCH'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
+}
+
+resource "aws_api_gateway_authorizer" "lambda_authorizer" {
+  name            = var.authorizer_name
+  rest_api_id     = aws_api_gateway_rest_api.rest_api.id
+  type            = "TOKEN"
+  authorizer_uri  = var.authorizer_uri
+  identity_source = "method.request.header.Authorization"
 }
